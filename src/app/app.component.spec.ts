881515d447281cef2fa06268d6051753af8c4c25
gonzoibox <gonzoibox@gmail.com>
2020-10-14 14:30:31 +0300

    fix project name - todo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo-Angular'`, () => {
+  it(`should have as title 'todo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-Angular');
+    expect(app.title).toEqual('todo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todo-Angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('todo app is running!');
   });
 });
